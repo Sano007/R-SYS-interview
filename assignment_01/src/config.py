9a0a3ce46da7cbbeef4b9f7a6e19394bcc6ce856
Samuel Králik <samuel.kralik@student.tuke.sk>
2024-11-29 16:00:36 +0100

    Created project configuration.

---
+++ assignment_01/src/config.py
@@ -0,0 +1,20 @@
+# 
+# Default file paths
+# 
+
+LOG_PATH			= "Logs/assignment01.log"
+JSON_MERGED_PATH	= "Output/merged-localizations.json"
+
+# 
+# Default URLs and moduls
+# 
+
+URL_MAIN_LOCALISATION_KEY = "https://ixoweb-pl.irsys.sk/ixo/locales/en.json"
+URL_MODUL_LOCALISATION_KEY = "https://ixoweb-pl.irsys.sk/ixo/js/bundles/"
+MODULS = {"aircraft", "animatedMapDataSource_wcs", "animatedMapLayers", "atfm", "catLayers", 
+		  "controlPanelMobile", "drawing", "exportroute", "featureInfo", "flFilter", 
+		  "flightlog", "flightmodel", "flightplans", "fplform", "fua", 
+		  "help", "imageViewer", "layerBrowser", "mapSearch", "measure", 
+		  "meteopib", "meteoViewer", "news", "nmb2bvalidation", "notamMsgsViewer", 
+		  "pams", "pib", "proposeRoute", "refresh", "sunriseSunset", 
+		  "userPoints", "userRegForm", "userSettings"}
